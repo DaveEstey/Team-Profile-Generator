2aea8efe387d99edd9d7f6c6cf0238235cdad104
Dave Estey <daveestey.working@gmail.com>
2023-01-16 02:43:46 -0400

    Created all the Classes that are being used and added methods to them

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require("inquirer");
+const employee = require("./lib/Employee");
+const engineer = require("./lib/Engineer");
+const intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
